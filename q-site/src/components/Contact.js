7c72c331cdfaa54230e62edabcade8987d7cf062
BaoWoW123 <baonguyen9392@gmail.com>
2025-08-11 22:47:21 -0700

    Replace Components(Msg, Appt) For Client Portal

--- q-site/src/components/Contact.js
+++ q-site/src/components/Contact.js
@@ -1,8 +1,6 @@
 import "../styles/Contact.css";
 import Footer from "./Footer";
-import { useForm, ValidationError } from "@formspree/react";
-import MsgForm from "./MsgForm";
-import ApptForm from "./ApptForm";
+import { useForm } from "@formspree/react";
 
 const Contact = () => {
   const [state, handleSubmit, reset] = useForm(
@@ -15,38 +13,29 @@ const Contact = () => {
 
   return (
     <div className="Contact">
+      <div style={{padding:' 0 10rem'}}>
       <h1>Contact</h1>
-      <div>
         Taking the first step can feel like the hardest part, but you don’t have
         to do it alone. Whether you have questions, need more information, or
         are ready to begin your counseling journey, we’re here to listen. We
         look forward to connecting with you.
       </div>
 
-      <h2>Book an appointment. </h2>
       <div className="contactSubsection">
+      <h2>Book an appointment </h2>
         <div>
-          <div>
             Think we can help? Take the first step in your journey and book a
             free phone consultation appointment today to see if we are a match.
-          </div>
-          <div>
-            <MsgForm />
-          </div>
         </div>
-        <div>
-          <div>
+        <div>          
             Ready to jump right into it? Book an initial intake appointment and
-            fill out the required documentation before your appointment.
+            fill out the required documentation before your appointment.          
           </div>
-          <div>
-            <ApptForm />
-          </div>
-        </div>
-        <div>
+          <button className="apptBtn" onClick={() => window.open('https://quynh-akers.clientsecure.me/', '_blank', 'noopener,noreferrer')}>Book an appointment</button>
+          <div className="apptNote">
           <b>Note:</b> Payment is due at the time of service. Please reach out
           for any questions regarding fees and lengths of services{" "}
-          <a href="https://www.google.com">here.</a>{" "}
+          <a href="https://quynh-akers.clientsecure.me/">here.</a>{" "}
           Q’s Counseling PLLC currently only offers telehealth appointments via
           a secured HIPAA compliant platform. We are in the process of getting
           credentialed with insurances and is only accepting self-pay
@@ -54,6 +43,7 @@ const Contact = () => {
           prefer to use your insurance. See you then!
         </div>
       </div>
+      
       <Footer />
     </div>
   );
